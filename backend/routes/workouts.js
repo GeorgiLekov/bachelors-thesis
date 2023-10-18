4b1c28f8d972e3f05c8d2bb3e923eafef4438878
georgi.lekov <georgi.lekov@scalefocus.com>
2023-10-18 23:34:45 +0300

    Add controllers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,43 +1,26 @@
 const express = require('express');
-const Workout = require('../models/workoutModel');
-
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
 const router = express.Router();
 
 // Get all workouts of user
-router.get('/', (req, res) => {
-    res.json({mssg: 'get all workouts'});
-});
+router.get('/', getWorkouts);
 
 // Get a single workout
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'get single workout'});
-});
+router.get('/:id', getWorkout);
 
 // Post a new workout
-router.post('/', async (req, res) => {
-    const { title, load, reps } = req.body;
-
-    try {
-        const workout = await Workout.create({
-            title,
-            reps,
-            load,
-        })
-        res.status(200).json(workout);
-    } catch(error) {
-        res.status(400).json({error: error});
-    }
-    res.json({mssg: 'post new workout'});
-});
+router.post('/', createWorkout);
 
 // Delete a workout
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'delete workout'});
-});
+router.delete('/:id', deleteWorkout);
 
 // Update a workout
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'update workout'});
-});
+router.patch('/:id', updateWorkout);
 
 module.exports = router;
